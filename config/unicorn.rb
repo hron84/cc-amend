36fe451c4658bdaf289e77d95f21cc37a80a9ef1
Garami Gábor <hron@hron.me>
2016-10-25 15:18:48 +0200

    Customize log and pid

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,5 @@
 ENV['NO_ROLLBAR'] = '1' unless ENV['ROLLBAR']
 
-stderr_path 'unicorn.log'
-stdout_path 'unicorn.log'
+stderr_path 'log/unicorn.log'
+stdout_path 'log/unicorn.log'
+pid 'tmp/pids/unicorn.pid'
